933066fa8dc231d8f39c4b99ae6ef07ca5fdfa18
Mike Resnik <mikatron3000@gmail.com>
2019-03-18 18:18:21 -0400

    Adds templates thanks to Hao.

---
+++ user_interface/urls.py
@@ -0,0 +1,22 @@
+"""WEBSITECORE URL Configuration
+
+"""
+from django.urls import path
+
+from . import views
+
+app_name = "WEBSITECORE"
+
+urlpatterns = [
+	path("", views.homepage, name="homepage"),
+	path("profile/edit", views.editProfile, name="editProfile"),
+	path("profile", views.profile, name="profile"),
+	path("profile/", views.profile, name="profile"),
+	path("register", views.register, name="register"),
+	path("register/", views.register, name="register"),
+	path("login", views.login, name="login"),
+	path("login/", views.login, name="login"),
+	path("search", views.search, name="search"),
+	path("search/", views.search, name="search"),
+	path("calendar/tests", views.calendarTest, name="calendarTest")
+]
