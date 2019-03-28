aa909c4d380b3727649d019216310ce1f9a16cc2
haozhang96 <haozhang@gmx.com>
2019-03-28 11:18:00 -0400

    Remove "/authentication" from URLs; fix login redirects

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 
 
 urlpatterns = [
-	path("", views.index, name="index"),
+	#path("", views.index, name="index"),
 	path("login", views.login, name="login"),
 	path("logout", views.logout, name="logout"),
 	path("logged_in", views.logged_in, name="logged_in"),
