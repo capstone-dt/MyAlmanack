68e93c7904cc3f76aca968ff732679b3fc13ca82
Mike Resnik <mikatron3000@gmail.com>
2019-04-01 20:48:30 -0400

    Changes views to a class hierarchy. Adds get() methods to each class.

--- user_interface/views.py
+++ user_interface/views.py
@@ -1,86 +1,69 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
 from django.http import HttpResponse
 import base64
 import os
 
-# In myalmanack folder
-# In main/views.py in sentdex tutorial
-# Create your views here.
-def homepage(request):
-	return render(
-		# Pass the request
-		request=request,
-		# Where to find template
-		template_name="user_interface/register.html",
-		# Pass in variable 'people' using Person.objects.all
-		context={}
-	)
-
-def editProfile(request):
-	return render(
-		# Pass the request
-		request=request,
-		# Where to find template
-		template_name="user_interface/edit_profile.html",
-		# Pass in variable 'people' using Person.objects.all
-		context={}
-	)
-
 def getProfilePictureBase64(file_name):
 	picture_dir = "\\user_interface\\static\\profile_pictures\\"
 	extension = ".png"
-	label = file_name
 	cwd = os.getcwd()
-	file_loc = cwd + picture_dir + label + extension
+	file_loc = cwd + picture_dir + file_name + extension
 	encoded_string = ""
 	with open(file_loc, "rb") as image_file:
 		encoded_string = base64.b64encode(image_file.read())
 	retval = encoded_string.decode('utf-8')
 	return retval
 
-def profile(request):
-	picture = getProfilePictureBase64("default_profile")
-	response = render(
-		request=request,
-		template_name="user_interface/profile.html",
-		context={'temp_image' : picture}
-	)
-	return response
+class ProfileView(TemplateView):
+	template_name = 'user_interface/profile.html'
 
-def group(request):
-	return render(
-		# Pass the request
-		request=request,
-		# Where to find template
-		template_name="user_interface/group.html",
-		# Pass in variable 'people' using Person.objects.all
-		context={}
-	)
+	def get(self, request):
+		response = render(
+			request=request,
+			template_name=self.template_name,
+			context={}
+		)
+		return response
 
-def register(request):
-	return render(
-	request = request,
-	template_name = "user_interface/register.html",
-	context={}
-	)
+class EditProfileView(TemplateView):
+	template_name = 'user_interface/edit_profile.html'
 
-def login(request):
-	return render(
-	request = request,
-	template_name = "user_interface/login.html",
-	context={}
-	)
+	def get(self, request):
+		response = render(
+			request=request,
+			template_name=self.template_name,
+			context={}
+		)
+		return response
 
-def search(request):
-	return render(
-	request = request,
-	template_name = "user_interface/search.html",
-	context={}
-	)
+class GroupView(TemplateView):
+	template_name = 'user_interface/group.html'
 
-def calendarTest(request):
-	return render(
-	request =request,
-	template_name = "user_interface/calendarTest.html",
-	context={}
-	)
+	def get(self, request):
+		return render(
+			request=request,
+			template_name=self.template_name,
+			context={}
+		)
+
+class DefaultView(TemplateView):
+	template_name = 'user_interface/default.html'
+
+	def get(self, request):
+		return render(
+			request=request,
+			template_name=self.template_name,
+			context={}
+		)
+
+class SearchView(TemplateView):
+	template_name = 'user_interface/search.html'
+
+	def get(self, request):
+		return render(
+			request=request,
+			template_name=self.template_name,
+			context={}
+		)
+
