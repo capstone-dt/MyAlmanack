4cc2884eeaf830c290bf95ef703fc4e828facca5
haozhang96 <haozhang@gmx.com>
2019-04-16 01:44:54 -0400

    Remove unnecessary policy check for action classes

--- authorization/decision/decision_authority.py
+++ authorization/decision/decision_authority.py
@@ -1,28 +1,19 @@
 from ..attributes.action import Action
 from ..enforcement.authorization_result import AuthorizationResult
-from ..utilities.reflection import get_class_name, assert_subclass
+from ..utilities.reflection import assert_subclass
 
 
 class DecisionAuthority:
     @classmethod
     def authorize(cls, request):
-        # Extract the action from the authorization request and make sure that
-        #     it is an Action class object.
-        action = request.action
-        assert_subclass(action, Action)
+        # Make sure that the authorization request's action is an Action class.
+        assert_subclass(request.action, Action)
         
-        # Make sure that policies have been provided by the action.
-        if not hasattr(action, "policies"):
-            raise NotImplementedError(
-                "The %s action has not provided any policies to evaluate!" %
-                get_class_name(action)
-            )
-        
-        print("Action:", action)
-        print("Policies:", action.policies)
+        print("Action:", request.action)
+        print("Policies:", request.action.policies)
         
         # Evaluate all the policies of this action.
-        for policy in action.policies:
+        for policy in request.action.policies:
             print("Evaluating policy:", policy)
             try:
                 if policy.evaluate(request):
