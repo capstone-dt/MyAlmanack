dec4e94efd89f8067c84d971e4aadac4f570de40
haozhang96 <haozhang@gmx.com>
2019-04-16 18:57:31 -0400

    Change the profile view to redirect to 404 if the user doesn't exist

--- authorization/views/profile.py
+++ authorization/views/profile.py
@@ -3,7 +3,7 @@ from authentication.firebase import get_session_claims
 
 # Django
 from django.contrib.auth import get_user_model
-from django.core.exceptions import PermissionDenied
+from django.http import Http404
 from django.shortcuts import render
 
 
@@ -12,7 +12,7 @@ def profile(request, uid):
     try:
         user = get_user_model().objects.get(username=uid)
     except get_user_model().DoesNotExist:
-        raise PermissionDenied
+        raise Http404
     
     # Check authorization.
     authorization.api.authorize(
