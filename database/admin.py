52667aed2730e775ff2de5882a289f41ca01e32f
haozhang96 <haozhang@gmx.com>
2019-04-24 21:20:20 -0400

    Fix database models (GroupInvite, GroupRequest)

--- database/admin.py
+++ database/admin.py
@@ -3,7 +3,7 @@ from .models import *
 # Register your models here.
 
 admin.site.register(ContactList)
-admin.site.register(Invite)
+#admin.site.register(Invite)
 admin.site.register(Event)
 admin.site.register(EventInvite)
 admin.site.register(Group)
